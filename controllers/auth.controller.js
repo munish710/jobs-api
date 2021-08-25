fd2624341e2a0cd2e3f2b35f4da70e8584a909dd
munish710 <munish710@gmail.com>
2021-08-26 01:40:32 +0530

    used mongoose middleware to hash passwords

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,19 +1,9 @@
 const User = require("../models/user.model");
 const { BadRequestError } = require("../errors");
-const bcrypt = require("bcryptjs");
 const { StatusCodes } = require("http-status-codes");
 
 const register = async (req, res) => {
-  const { name, email, password } = req.body;
-  if (!name || !email || !password) {
-    throw new BadRequestError("Please provide name,email and password.");
-  }
-
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password, salt);
-
-  const tempUser = { email, name, password: hashedPassword };
-  const user = await User.create({ ...tempUser });
+  const user = await User.create({ ...req.body });
   res.status(StatusCodes.CREATED).json(user);
 };
 
--- models/user.model.js
+++ models/user.model.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -20,6 +22,14 @@ const UserSchema = new mongoose.Schema({
     required: [true, "Pease provide a name"],
     minLength: 6,
   },
+});
+
+//use function keyword, becasue this refers to document.
+//next usage is nt necessary-read DOCS
+UserSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
 module.exports = mongoose.model("User", UserSchema);
