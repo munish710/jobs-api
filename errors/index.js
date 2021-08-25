c9e1e9268c4cc679cd2f813ef5f6c7bf5fab7758
munish710 <munish710@gmail.com>
2021-08-25 22:50:41 +0530

    basic express app setup

---
+++ errors/index.js
@@ -0,0 +1,11 @@
+const CustomAPIError = require("./custom-api");
+const UnauthorizedError = require("./unauthorized");
+const NotFoundError = require("./not-found");
+const BadRequestError = require("./bad-request");
+
+module.exports = {
+  CustomAPIError,
+  UnauthorizedError,
+  NotFoundError,
+  BadRequestError,
+};
