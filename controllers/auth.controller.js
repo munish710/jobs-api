0030fd6cacfbc186247ac815efc329b0cbe816d4
munish710 <munish710@gmail.com>
2021-08-26 01:24:28 +0530

    added register user functionlaity

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,20 @@
+const User = require("../models/user.model");
+const { BadRequestError } = require("../errors");
+const bcrypt = require("bcryptjs");
+const { StatusCodes } = require("http-status-codes");
+
 const register = async (req, res) => {
-  res.send("register user");
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    throw new BadRequestError("Please provide name,email and password.");
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const tempUser = { email, name, password: hashedPassword };
+  const user = await User.create({ ...tempUser });
+  res.status(StatusCodes.CREATED).json(user);
 };
 
 const login = async (req, res) => {
--- models/user.model.js
+++ models/user.model.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoosse");
+const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
